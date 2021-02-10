c0dafa01a6e09df787471d136de06e38d12c0b48
Adil <adil@fit.ba>
2021-02-10 12:34:30 +0100

    SPA

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# spa-2020-21
+# asp-2016-17
+C++ code iz predmeta "Algorimi i strukture podataka" - vježbe - ak. 2016/17
+
+
+		cout << "1: ATP Lista" << endl;
+		cout << "2: ATP Stek" << endl;
+		cout << "3: ATP Red" << endl;
+		cout << "4: ATP Prioritetni red" << endl;
+		cout << "5: Algoritmi sortiranja n^2 (SelectionSort i InsertionSort)" << endl;
+		cout << "6: Algoritmi sortiranja n*logn (QuckSort i MergeSort)" << endl;
+		cout << "7: Matrica susjedstva" << endl;
+		cout << "8: Binary Search Tree (konstrukcija, pretraga) i Binary Tree obilazak" << endl;
+		cout << "9: Algoritmi za obilazak grafa (Bfs i Dfs)" << endl;
+		cout << "10: Algoritmi najkrace putanje (Dijkstra) " << endl;
+		cout << "11: Algoritmi stabla minimalnog raspona (Prim i Kruskal)" << endl;
+		cout << "12: Hashing" << endl;
+
+video lekcije
+https://www.youtube.com/playlist?list=PLJCjqoTZy0H8cBAswoI5v2Mwui-6a4kte
+
+Heširanje
+https://www.youtube.com/watch?v=8gEa-duZEsc&t=4s
+
+Heap Sort
+https://www.youtube.com/watch?v=aQtV_ynBbbw&t=1s 
+
+Radix Sort
+https://www.youtube.com/watch?v=sa5rySL6l20
+
+Bucket Sort
+https://www.youtube.com/watch?v=iUVRl-wUj5k&t=1026s 
+
+AVL Stablo
+https://youtu.be/deDeIXMwUF8
---
+++ code/ASP/HashiranjeOtvorenoAdresiranje.h
@@ -0,0 +1,91 @@
+#pragma once
+#include "Hashiranje.h"
+#include <exception>
+#include <iostream>
+
+template <class Tip>
+class HeshiranjeOtvorenoAdresiranje :public Hashiranje<Tip>
+{
+	Tip* Niz;
+	int n;
+	bool* zauzeto;
+	int IzracunajHash(Tip k)
+	{
+		return k % n;
+	}
+	int brojac;
+public:
+	HeshiranjeOtvorenoAdresiranje(int n = 10)
+	{
+		this->n = n;
+		Niz = new Tip[n];
+		zauzeto = new bool[n];
+		for (int i = 0; i<n; i++)
+		{
+			zauzeto[i] = false;
+		}
+		brojac = 0;
+	}
+	bool jelPun()
+	{
+		return brojac == n;
+	}
+	void dodaj(Tip k)
+	{
+		if (jelPun())
+			throw exception("greska. Hashset je pun.");
+		int i = IzracunajHash(k);
+
+		while (zauzeto[i])
+		{
+			i++;
+			if (i == n)
+				i = 0;
+		}
+		Niz[i] = k;
+		zauzeto[i] = true;
+		brojac++;
+	}
+
+	bool jelPrazan()
+	{
+		return brojac = 0;
+	}
+
+	bool ukloni(Tip k)
+	{
+		if (jelPrazan())
+			return false;
+
+		int i = IzracunajHash(k);
+		int b = 0;
+		while (b != n)
+		{
+			if (zauzeto[i])
+			{
+				if (Niz[i] == k)
+				{
+					zauzeto[i] = false;
+					cout << "uklonjeno " << k << endl;
+					brojac--;
+					return true;
+				}
+			}
+			i++;
+			b++;
+			if (i == n)
+				i = 0;
+		}
+		return false;
+	}
+	void print()
+	{
+		for (int i = 0; i<n; i++)
+		{
+			if (zauzeto[i])
+				cout << i << ": " << Niz[i] << endl;
+			else
+				cout << i << ": (x) " << endl;
+		}
+	}
+};
